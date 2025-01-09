39ccd1b79e9d7c316e2e8044513cbab04164e67d
Thomas Ferte <thomas.ferte@u-bordeaux.fr>
2025-01-09 18:33:53 +0100

    add slurm experience launcher

--- slurm_experiments/generate_slurm_files.R
+++ slurm_experiments/generate_slurm_files.R
@@ -1,35 +1,37 @@
+library(dplyr)
+
 ##### GeneticSingleIs_GA_GAHPDEF #####
-generate_slurm_files <- function(folder, pmutQuant, pmutCat, sigmahalv, delayed){
-  slurm_scenari <- glue::glue("GeneticSingleIs_GA_GAHPDEF_pmutQuant{pmutQuant*1000}_pmutCat{pmutCat*1000}_sigmahalv{sigmahalv*1000}")
+generate_slurm_exp1 <- function(folder, method, history, features, delayed) {
+  slurm_scenari <- glue::glue("method_{method}_history_{history}_features_{features}")
   res <- glue::glue(
     '#!/bin/bash
-  
+
   #############################
   # les directives Slurm vont ici:
-  
+
   # Your job name (displayed by the queue)
   #SBATCH -J {slurm_scenari}
-  
+
   # walltime (hh:mm::ss)
   #SBATCH -t 06:30:00
   #SBATCH --begin=now+{delayed}hours
-  
-  
+
+
   # change working directory
   # SBATCH --chdir=.
-  
+
   ### In filenames, %j=jobid, %a=index in job array
   #SBATCH -o /beegfs/tferte/std_out/%j_%a_%x.out # standard out goes to this file
   #SBATCH -e /beegfs/tferte/std_err/%j_%a_%x.err # standard err goes to this file
-  
+
   #SBATCH --array 0-199
   #SBATCH --ntasks 1
   #SBATCH --cpus-per-task 5
   #SBATCH --mem-per-cpu=2560
-  
+
   # fin des directives PBS
   #############################
-  
+
   # useful informations to print
   echo "#############################"
   echo "User:" $USER
@@ -42,61 +44,61 @@ generate_slurm_files <- function(folder, pmutQuant, pmutCat, sigmahalv, delayed)
   echo "SLURM_JOB_NODELIST:" $SLURM_JOB_NODELIST
   echo "SLURM_JOB_NAME" : $SLURM_JOB_NAME
   echo "#############################"
-  
+
   #############################
   export OPENBLAS_NUM_THREADS=1
   export OMP_NUM_THREADS=1
   export MKL_NUM_THREADS=1
-  
-  # cd /gpfs/home/tferte910e/high_dimension_reservoir
-  # source /gpfs/home/tferte910e/predictcovid_api_python/testenv/bin/activate
-  
+
   module load build/conda/4.10
   conda activate testenv
-  cd /home/tferte/ChallengesRCGA
-  
+  cd /home/tferte/EpidemioForecast
+
   ulimit -c 0
-  
+
   python main_csv_evaluate.py
   '
   )
-  
-  write(x = res, file = glue::glue("{folder}/{slurm_scenari}.slurm"))
-  
+
+  write(
+    x = res,
+    file = here::here(glue::glue("{folder}/{slurm_scenari}.slurm"))
+  )
+
   return(NULL)
 }
 
-generate_slurm_files_test <- function(folder, pmutQuant, pmutCat, sigmahalv, delayed){
-  slurm_scenari <- glue::glue("GeneticSingleIs_GA_GAHPDEF_pmutQuant{pmutQuant*1000}_pmutCat{pmutCat*1000}_sigmahalv{sigmahalv*1000}")
-  
+generate_slurm_exp1_test <- function(folder, method, history, features, delayed) {
+  slurm_scenari <- glue::glue("method_{method}_history_{history}_features_{features}")
+
   res <- glue::glue(
     '#!/bin/bash
-    
+
     #############################
     # les directives Slurm vont ici:
-    
+
     # Your job name (displayed by the queue)
     #SBATCH -J {slurm_scenari}
-    
+
     # walltime (hh:mm::ss)
     #SBATCH -t 15:00:00
-    
-    
+
+
     # change working directory
     # SBATCH --chdir=.
-    
+
     ### In filenames, %j=jobid, %a=index in job array
     #SBATCH -o /beegfs/tferte/std_out/%j_%a_%x.out # standard out goes to this file
     #SBATCH -e /beegfs/tferte/std_err/%j_%a_%x.err # standard err goes to this file
-    
+
     #SBATCH --array 0-10
     #SBATCH --ntasks 1
     #SBATCH --cpus-per-task 10
     #SBATCH --mem-per-cpu=2560
-    
+
     # fin des directives PBS
     #############################
-    
+
     # useful informations to print
     echo "#############################"
     echo "User:" $USER
@@ -109,210 +111,62 @@ generate_slurm_files_test <- function(folder, pmutQuant, pmutCat, sigmahalv, del
     echo "SLURM_JOB_NODELIST:" $SLURM_JOB_NODELIST
     echo "SLURM_JOB_NAME" : $SLURM_JOB_NAME
     echo "#############################"
-    
+
     #############################
     export OPENBLAS_NUM_THREADS=1
     export OMP_NUM_THREADS=1
     export MKL_NUM_THREADS=1
-    
-    # cd /gpfs/home/tferte910e/high_dimension_reservoir
-    # source /gpfs/home/tferte910e/predictcovid_api_python/testenv/bin/activate
-    
+
     module load build/conda/4.10
     conda activate testenv
-    cd /home/tferte/ChallengesRCGA
-    
+    cd /home/tferte/EpidemioForecast
+
     ulimit -c 0
-    
+
     python main_csv_test.py'
   )
-  
-  write(x = res, file = glue::glue("{folder}/{slurm_scenari}.slurm"))
-  
-  return(NULL)
-}
 
-df_exp1 <- expand.grid(pmutCat = c(0.016, 0.04, 0.1, 0.25),
-                       sigmahalv = c(0.1, 0.2, 0.4, 0.8)) |> 
-  tibble::rowid_to_column(var = "delayed")
-
-df_exp1 |> 
-  apply(MARGIN = 1,
-        FUN = function(row){
-          generate_slurm_files(folder = "slurm_experiments/explore_ga_exp1",
-                               pmutQuant = 0.5,
-                               pmutCat = as.numeric(row["pmutCat"]),
-                               sigmahalv = as.numeric(row["sigmahalv"]),
-                               delayed = (as.numeric(row["delayed"])-1)*3)
-        })
-
-
-df_exp1 |> 
-  apply(MARGIN = 1,
-        FUN = function(row){
-          generate_slurm_files_test(folder = "slurm_experiments/explore_ga_exp1_test",
-                                    pmutQuant = 0.5,
-                                    pmutCat = as.numeric(row["pmutCat"]),
-                                    sigmahalv = as.numeric(row["sigmahalv"]),
-                                    delayed = (as.numeric(row["delayed"])-1)*3)
-        })
-
-
-##### GeneticSingleIs_GA_GAPENAL #####
-generate_slurm_files_penal <- function(folder, NbFeaturesPenalty, TournamentFeaturesPenalty, delayed){
-  slurm_scenari <- glue::glue("GeneticSingleIs_GA_GAPENAL_NbFeaturesPenalty{NbFeaturesPenalty*1000}_TournamentFeaturesPenalty{TournamentFeaturesPenalty}")
-  res <- glue::glue(
-    '#!/bin/bash
-  
-  #############################
-  # les directives Slurm vont ici:
-  
-  # Your job name (displayed by the queue)
-  #SBATCH -J {slurm_scenari}
-  
-  # walltime (hh:mm::ss)
-  #SBATCH -t 06:30:00
-  #SBATCH --begin=now+{delayed}hours
-  
-  
-  # change working directory
-  # SBATCH --chdir=.
-  
-  ### In filenames, %j=jobid, %a=index in job array
-  #SBATCH -o /beegfs/tferte/std_out/%j_%a_%x.out # standard out goes to this file
-  #SBATCH -e /beegfs/tferte/std_err/%j_%a_%x.err # standard err goes to this file
-  
-  #SBATCH --array 0-199
-  #SBATCH --ntasks 1
-  #SBATCH --cpus-per-task 5
-  #SBATCH --mem-per-cpu=2560
-  
-  # fin des directives PBS
-  #############################
-  
-  # useful informations to print
-  echo "#############################"
-  echo "User:" $USER
-  echo "Date:" `date`
-  echo "Host:" `hostname`
-  echo "Directory:" `pwd`
-  echo "SLURM_JOBID:" $SLURM_JOB_ID
-  echo "SLURM_ARRAY_JOB_ID:" $SLURM_ARRAY_JOB_ID
-  echo "SLURM_SUBMIT_DIR:" $SLURM_SUBMIT_DIR
-  echo "SLURM_JOB_NODELIST:" $SLURM_JOB_NODELIST
-  echo "SLURM_JOB_NAME" : $SLURM_JOB_NAME
-  echo "#############################"
-  
-  #############################
-  export OPENBLAS_NUM_THREADS=1
-  export OMP_NUM_THREADS=1
-  export MKL_NUM_THREADS=1
-  
-  # cd /gpfs/home/tferte910e/high_dimension_reservoir
-  # source /gpfs/home/tferte910e/predictcovid_api_python/testenv/bin/activate
-  
-  module load build/conda/4.10
-  conda activate testenv
-  cd /home/tferte/ChallengesRCGA
-  
-  ulimit -c 0
-  
-  python main_csv_evaluate.py
-  '
+  write(
+    x = res,
+    file = here::here(glue::glue("{folder}/{slurm_scenari}.slurm"))
   )
-  
-  write(x = res, file = glue::glue("{folder}/{slurm_scenari}.slurm"))
-  
+
   return(NULL)
 }
 
-generate_slurm_files_test_penal <- function(folder, NbFeaturesPenalty, TournamentFeaturesPenalty, delayed){
-  slurm_scenari <- glue::glue("GeneticSingleIs_GA_GAPENAL_NbFeaturesPenalty{NbFeaturesPenalty*1000}_TournamentFeaturesPenalty{TournamentFeaturesPenalty}")
-  
-  res <- glue::glue(
-    '#!/bin/bash
-    
-    #############################
-    # les directives Slurm vont ici:
-    
-    # Your job name (displayed by the queue)
-    #SBATCH -J {slurm_scenari}
-    
-    # walltime (hh:mm::ss)
-    #SBATCH -t 15:00:00
-    
-    
-    # change working directory
-    # SBATCH --chdir=.
-    
-    ### In filenames, %j=jobid, %a=index in job array
-    #SBATCH -o /beegfs/tferte/std_out/%j_%a_%x.out # standard out goes to this file
-    #SBATCH -e /beegfs/tferte/std_err/%j_%a_%x.err # standard err goes to this file
-    
-    #SBATCH --array 0-10
-    #SBATCH --ntasks 1
-    #SBATCH --cpus-per-task 10
-    #SBATCH --mem-per-cpu=2560
-    
-    # fin des directives PBS
-    #############################
-    
-    # useful informations to print
-    echo "#############################"
-    echo "User:" $USER
-    echo "Date:" `date`
-    echo "Host:" `hostname`
-    echo "Directory:" `pwd`
-    echo "SLURM_JOBID:" $SLURM_JOB_ID
-    echo "SLURM_ARRAY_JOB_ID:" $SLURM_ARRAY_JOB_ID
-    echo "SLURM_SUBMIT_DIR:" $SLURM_SUBMIT_DIR
-    echo "SLURM_JOB_NODELIST:" $SLURM_JOB_NODELIST
-    echo "SLURM_JOB_NAME" : $SLURM_JOB_NAME
-    echo "#############################"
-    
-    #############################
-    export OPENBLAS_NUM_THREADS=1
-    export OMP_NUM_THREADS=1
-    export MKL_NUM_THREADS=1
-    
-    # cd /gpfs/home/tferte910e/high_dimension_reservoir
-    # source /gpfs/home/tferte910e/predictcovid_api_python/testenv/bin/activate
-    
-    module load build/conda/4.10
-    conda activate testenv
-    cd /home/tferte/ChallengesRCGA
-    
-    ulimit -c 0
-    
-    python main_csv_test.py'
+df_exp1 <- list(
+  method = c("enet", "xgboost", "reservoir"),
+  history = c(365, 90),
+  features = c("epi", "all")
+) %>%
+  expand.grid() %>%
+  tibble::rowid_to_column("delayed") %>%
+  dplyr::mutate(delayed = delayed * 4)
+
+df_exp1 %>%
+  apply(
+    MARGIN = 1,
+    FUN = function(row) {
+      generate_slurm_exp1(
+        folder = "slurm_experiments/experience1",
+        method = row["method"],
+        history = as.numeric(row["history"]),
+        features = row["features"],
+        delayed = as.numeric(row["delayed"])
+      )
+    }
   )
-  
-  write(x = res, file = glue::glue("{folder}/{slurm_scenari}.slurm"))
-  
-  return(NULL)
-}
 
-df_exp2 <- data.frame(NbFeaturesPenalty = c(0, 1e-3, 5*1e-3, 1e-2),
-                      TournamentFeaturesPenalty = c(1, rep(0, 3))) |> 
-  tibble::rowid_to_column(var = "delayed")
-
-df_exp2 |> 
-  apply(MARGIN = 1,
-        FUN = function(row){
-          generate_slurm_files_penal(folder = "slurm_experiments/explore_ga_exp2",
-                                     NbFeaturesPenalty = as.numeric(row["NbFeaturesPenalty"]),
-                                     TournamentFeaturesPenalty = as.numeric(row["TournamentFeaturesPenalty"]),
-                                     delayed = (as.numeric(row["delayed"])-1)*3)
-        })
-
-
-df_exp2 |> 
-  apply(MARGIN = 1,
-        FUN = function(row){
-          generate_slurm_files_test_penal(folder = "slurm_experiments/explore_ga_exp2_test",
-                                          NbFeaturesPenalty = as.numeric(row["NbFeaturesPenalty"]),
-                                          TournamentFeaturesPenalty = as.numeric(row["TournamentFeaturesPenalty"]),
-                                          delayed = (as.numeric(row["delayed"])-1)*3)
-        })
-
-
+df_exp1 %>%
+  apply(
+    MARGIN = 1,
+    FUN = function(row) {
+      generate_slurm_exp1_test(
+        folder = "slurm_experiments/experience1_test",
+        method = row["method"],
+        history = as.numeric(row["history"]),
+        features = row["features"],
+        delayed = as.numeric(row["delayed"])
+      )
+    }
+  )
